2375978c4e89b6e7bfc019d1212ae4efb2291b54
Ilias Stamatis <stamatis.iliass@gmail.com>
2020-10-03 17:48:42 +0300

    map E1000's internal registers in the kernel address space

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,8 +1,14 @@
 #include <kern/e1000.h>
+#include <kern/pmap.h>
+
+volatile void *reg_base; /* corresponds to BAR[0] */
+
 
 // LAB 6: Your driver code here
 int e1000_attach(struct pci_func *pcif)
 {
 	pci_func_enable(pcif);
+	reg_base = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+
 	return 0;
 }
