501dce59a7a766ca226ecaa4dffae61435a93c6f
Ilias Stamatis <stamatis.iliass@gmail.com>
2020-10-03 21:29:08 +0300

    perform transmit initialization

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,14 +1,39 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 
+#define E1000_REG(offset) (*(volatile uint32_t *)(reg_base + offset))
+
+struct e1000_tx_desc tx_queue[NUM_TX_DESC] __attribute__((aligned(16)));
+char tx_buffers[NUM_TX_DESC][MAX_TX_BUF_SIZE];
+
+
 volatile void *reg_base; /* corresponds to BAR[0] */
 
 
-// LAB 6: Your driver code here
 int e1000_attach(struct pci_func *pcif)
 {
 	pci_func_enable(pcif);
 	reg_base = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+
+	/* initialize the transmit descriptors */
+	for (int i = 0; i < NUM_TX_DESC; i++) {
+		tx_queue[i].addr = PADDR(tx_buffers[i]);
+	}
+
+	/* transmit initialization */
+
+	E1000_REG(E1000_TDBAH) = 0;
+	E1000_REG(E1000_TDBAL) = PADDR(tx_queue);
+
+	E1000_REG(E1000_TDLEN) = sizeof(tx_queue);
+
+	E1000_REG(E1000_TDH) = 0;
+	E1000_REG(E1000_TDT) = 0;
+
+	E1000_REG(E1000_TCTL) = 0 | E1000_TCTL_EN | E1000_TCTL_PSP;
+	E1000_REG(E1000_TCTL) |= (E1000_TCTL_COLD & (0x40 << 12));
+
+	E1000_REG(E1000_TIPG) = 0 | 10 | 4 << 10 | 6 << 20;
 
 	return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -6,6 +6,33 @@
 #define E1000_VEN_ID           0x8086
 #define E1000_DEV_ID_82540EM   0x100E
 
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_COLD   0x003ff000    /* collision distance */
+
+
+#define MAX_TX_BUF_SIZE  1518
+#define NUM_TX_DESC        64 /* Number of transmit descriptors -
+                                 JOS's tests require no more than 64*/
+
+/* Legacy Transmit Descriptor */
+struct e1000_tx_desc {
+	uint64_t addr;
+	uint16_t length;
+	uint8_t cso;
+	uint8_t cmd;
+	uint8_t status;
+	uint8_t css;
+	uint16_t special;
+} __attribute__((packed));
 
 int e1000_attach(struct pci_func *pcif);
 
