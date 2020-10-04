cf68b585575fac262d5105419ed1039e9495965d
Ilias Stamatis <stamatis.iliass@gmail.com>
2020-10-04 14:50:28 +0300

    implement the main loop of the net-output environment

--- net/output.c
+++ net/output.c
@@ -10,4 +10,14 @@ output(envid_t ns_envid)
 	// LAB 6: Your code here:
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
+
+	while (1) {
+		int request = ipc_recv(NULL, &nsipcbuf, NULL);
+
+		if (request != NSREQ_OUTPUT)
+			continue;
+
+		/* in case of any error simply drop the packet */
+		sys_transmit_frame(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len);
+	}
 }
